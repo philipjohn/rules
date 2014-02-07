ee8d7c59eceee804a396b6471457f90e112df94c
Philip John <phil@philipjohn.co.uk>
2014-02-07 18:11:18 +0000

    Forget Composer for now, move ExtendedCPTs

--- rules.php
+++ rules.php
@@ -9,3 +9,71 @@
 	Text Domain: rules
 	Domain Path: /languages
  */
+
+Class Rules {
+	
+	/**
+	 * In the beginning, Phil created a Class...
+	 */
+	function __construct() {
+		
+		// (De-)activation hooks
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+		
+		// Grab the required libraries
+		require_once 'lib/extendedcpts/extended-cpts.php'; // Extended CPTs
+		
+		// Set up...
+		self::actions(); // Actions
+		self::filters(); // and Filters
+		
+	}
+	
+	/**
+	 * WP plugin activation hook
+	 */
+	function activate() {
+		// Do nothing, for now
+	}
+	
+	/**
+	 * WP plugin deactivation hook
+	 */
+	function deactivate() {
+		// Do nothing, for now
+	}
+	
+	/**
+	 * Register WP action hooks
+	 */
+	function actions() {
+		
+		add_action( 'init', array( $this, 'cpts' ) ); // Custom Post Types
+		
+	}
+	
+	/**
+	 * Register WP filter hooks
+	 */
+	function filters() {
+		
+		
+		
+	}
+	
+	function cpts() {
+		
+		register_extended_post_type( 'rule', array(
+			
+			'menu_icon' => 'dashicons-book',
+			
+			'menu_position' => 65
+		
+		) );
+		
+	}
+	
+}
+
+new Rules;
