bd2aedf49948f8d15146bc42e22bcc939cc4b7f2
Philip John <phil@philipjohn.co.uk>
2014-02-07 19:42:29 +0000

    Replace ACF with Meta Box

--- rules.php
+++ rules.php
@@ -22,14 +22,14 @@ Class Rules {
 		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
 		
 		# Grab the required libraries
-		require_once 'lib/extendedcpts/extended-cpts.php'; # Extended CPTs
-		
-		# Set up ACF, hide from normal users
-		if ( !defined('WP_LOCAL_DEV') ) {
-			define( 'ACF_LITE' , true );
+		if ( file_exists('lib/extendedcpts/extended-cpts.php') ) {
+			require_once 'lib/extendedcpts/extended-cpts.php'; # Extended CPTs
+		} else {
+			wp_die( __('Oops, a required library (Extended CPTs) appears to be missing!') );
 		}
-		require_once 'lib/advanced-custom-fields/acf.php';
 		
+		# Set up Meta Box
+		self::meta_box();		
 		
 		# Set up...
 		self::actions(); # Actions
@@ -41,14 +41,14 @@ Class Rules {
 	 * WP plugin activation hook
 	 */
 	function activate() {
-		# Do nothing, for now
+		# "Somewhere, something incredible is waiting to be known."
 	}
 	
 	/**
 	 * WP plugin deactivation hook
 	 */
 	function deactivate() {
-		# Do nothing, for now
+		# "The universe is not required to be in perfect harmony with human ambition."
 	}
 	
 	/**
@@ -58,6 +58,22 @@ Class Rules {
 		
 		add_action( 'init', array( $this, 'cpts' ) ); # Custom Post Types
 		add_action( 'admin_menu', array( $this, 'remove_meta_boxes' ) );
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) ); # Guess...
+		
+	}
+	
+	function meta_box() {
+		
+		# Tell Meta Box where it is
+		define( 'RWMB_URL', trailingslashit( dirname(__FILE__) . '/lib/meta-box' ) );
+		define( 'RWMB_DIR', trailingslashit( dirname(__FILE__) . '/lib/meta-box' ) );
+		
+		# Require it, fail if it's disappeared
+		if ( file_exists( RWMB_DIR . 'meta-box.php') ) {
+			require_once RWMB_DIR . 'meta-box.php';
+		} else {
+			wp_die( __('Oops, a required library (Meta Box) appears to be missing!') );
+		}
 		
 	}
 	
@@ -65,9 +81,7 @@ Class Rules {
 	 * Register WP filter hooks
 	 */
 	function filters() {
-		
-		
-		
+		# “In all our searching, the only thing we've found that makes the emptiness bearable is each other.”
 	}
 	
 	/**
@@ -100,6 +114,14 @@ Class Rules {
 			remove_meta_box( 'authordiv', 'rule', 'normal' ); # Author
 			remove_meta_box( 'pageparentdiv', 'rule', 'side' ); # Page Attributes
 		}
+	}
+	
+	function add_meta_boxes() {
+		add_meta_box( 'information', __('Information'), array( $this, 'meta_box_information' ), 'rule', 'normal', 'core' );
+	}
+	
+	function meta_box_information( $post ) {
+		echo 'Hello, world!';
 	}
 	
 }
